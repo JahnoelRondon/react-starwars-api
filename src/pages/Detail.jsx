b8f91eb5a730c3fb4d7bc507d37db832c64e8714
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-07 14:45:30 -0500

    added state props to fill in the detail for the shipdetail route componenet

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,21 @@
 import { useLocation } from "react-router"
 
 function Detail(){
-    let location = useLocation().state.ship;
+    let ship = useLocation().state.ship;
 
     return(
-        <h1>detail page</h1>
+        <div className='detail'>
+            <h1>{ship.name}</h1>
+            <p><span>Manufacturer:</span> {ship.manufacturer}</p>
+            <p><span>Model:</span> {ship.model}</p>
+            <p><span>Class:</span> {ship.starship_class}</p>
+            <br/>
+            <h3>Technical Specifics</h3>
+            <p><span>Length:</span> {ship.length} meters</p>
+            <p><span>Crew:</span> {ship.crew}</p>
+            <p><span>Passengers:</span> {ship.passengers}</p>
+            <p><span>Maximum atmospheric speed:</span> {ship.max_atmosphering_speed} kph</p>
+        </div>
     )
 }
 
