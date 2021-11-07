c9cc421f039af75019936314e9bfa8ea64c06153
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-07 14:57:13 -0500

    added styling to detail page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -9,12 +9,14 @@ function Detail(){
             <p><span>Manufacturer:</span> {ship.manufacturer}</p>
             <p><span>Model:</span> {ship.model}</p>
             <p><span>Class:</span> {ship.starship_class}</p>
+            <p><span>Cost in Credits:</span> {ship.cost_in_credits}</p>
             <br/>
             <h3>Technical Specifics</h3>
             <p><span>Length:</span> {ship.length} meters</p>
             <p><span>Crew:</span> {ship.crew}</p>
             <p><span>Passengers:</span> {ship.passengers}</p>
             <p><span>Maximum atmospheric speed:</span> {ship.max_atmosphering_speed} kph</p>
+            <a href="/">Return</a>
         </div>
     )
 }
